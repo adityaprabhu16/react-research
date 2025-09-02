9c72835bf12c8991e8cfe6ce95c165a764cddc3d
Aditya Prabhu <90486414+adityaprabhu16@users.noreply.github.com>
2025-09-02 17:47:44 -0500

    added DOM tutorial

---
+++ dom-tutorial/app.js
@@ -0,0 +1,10 @@
+const search = document.getElementById('search-books');
+const bookList = document.getElementById('book-list');
+
+var titles = document.getElementsByClassName('title');
+
+console.log(Array.isArray(titles));
+
+
+var wiseManQuote = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wiseManQuote);
