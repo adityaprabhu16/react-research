97b21018939845a37bb68e785bd9846f7ad5ec02
Aditya Prabhu <90486414+adityaprabhu16@users.noreply.github.com>
2025-09-06 17:54:52 -0500

    used updated todos manager

--- todos2/src/Todos.jsx
+++ todos2/src/Todos.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react';
 
+import TodosManager from './TodosManager';
+
 import './index.css';
 
+const todosManager = TodosManager();
 
 function Todos() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(todosManager.getTodos())
   const [todoInput, setTodoInput] = useState("");
 
   function handleInputChange(event) {
     setTodoInput(event.target.value);
   }
 
-  function addTodo(input) {
-    if(input.trim()) {
-      setTodos([...todos, input]);
+  function addTodo(title) {
+    if(title.trim()) {
+      todosManager.addTodo(title);
+      setTodos(todosManager.getTodos());
     }
     setTodoInput("");
   }
@@ -21,8 +25,8 @@ function Todos() {
   function deleteTodo(idx) {
     //we can naturally destructure by index:
     //const updatedTodos = todos.filter((element, index) => idx !== index);
-    const updatedTodos = todos.filter((_, index) => idx !== index);
-    setTodos(updatedTodos);
+    todosManager.deleteTodo(idx);
+    setTodos(todosManager.getTodos); 
   }
 
   function updateTodo(idx) {
---
+++ todos2/src/TodosManager.js
@@ -0,0 +1,40 @@
+
+class TodosManager {
+    constructor () {
+        this.todos = [];
+        this.currId = -1;
+    }
+
+    addTodo(title) {
+        this.currId += 1;
+        const newTodo = {id: this.currId, title: title};
+        this.todos.push(newTodo);
+        //helpful for unit testing.
+        return newTodo;
+    }
+
+    //if we have multiple updates, this would work perfectly.
+    //updates = {title}, so ...{title} = {title: title}
+    updateTodo(id, updates) {
+        //confirm id safe
+        if (id === -1) throw new Error("Invalid todo id!");
+        //updates will take precedence here. 
+        this.todos[id] = {...this.todos[id], ...updates}; 
+        //useful for unit testing: confirm we updated correctly.
+        return this.todos[id];
+    }
+
+    deleteTodo(id) {
+        if (id === -1) throw new Error("Invalid todo id");
+        //we'll use find so we can grab deletedTodo for unit testing
+        const deletedTodo = this.todos.find((t) => t.id === id);
+        this.todos = this.todos.filter((t) => t.id !== id);
+        return deletedTodo;
+    }
+
+    getTodos() {
+        return [...this.todos];
+    }
+}
+
+export default TodosManager;
